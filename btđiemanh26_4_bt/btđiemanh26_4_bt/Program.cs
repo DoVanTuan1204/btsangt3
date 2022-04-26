92080b3824b02802b27f1449edbb99d8db5d6e66
longbachviet <longbachviet@gmail.com>
2022-04-26 08:18:03 +0700

    a

--- btđiemanh26_4_bt/btđiemanh26_4_bt/Program.cs
+++ btđiemanh26_4_bt/btđiemanh26_4_bt/Program.cs
@@ -8,26 +8,26 @@ namespace btđiemanh26_4_bt
         {
           
                 Console.WriteLine("Nhap a,b,c: ");
-                double a210 = Int32.Parse(Console.ReadLine());
-                double b210 = Int32.Parse(Console.ReadLine());
-                double c210 = Int32.Parse(Console.ReadLine());
-                ptb2 pt = new ptb2(a210, b210, c210);
+                double a255 = Int32.Parse(Console.ReadLine());
+                double b255 = Int32.Parse(Console.ReadLine());
+                double c255 = Int32.Parse(Console.ReadLine());
+                ptb2 pt = new ptb2(a255, b255, c255);
 
-                double delta = pt.delta(a210, b210, c210);
-                int check210 = pt.nghiem(delta);
-                if (check210 < 0)
+                double delta = pt.delta(a255, b255, c255);
+                int check255 = pt.nghiem(delta);
+                if (check255 < 0)
                 {
                     Console.WriteLine("Pt vo nghiem");
                 }
-                else if (check210 == 0)
+                else if (check255 == 0)
                 {
-                    double x = -b210 / (2 * a210);
+                    double x = -b255 / (2 * a255);
                     Console.WriteLine("pt co nghiem kep: ", x);
                 }
                 else
                 {
-                    double x1 = (-b210 - Math.Sqrt(delta)) / (2 * a210);
-                    double x2 = (-b210 + Math.Sqrt(delta)) / (2 * a210);
+                    double x1 = (-b255 - Math.Sqrt(delta)) / (2 * a255);
+                    double x2 = (-b255 + Math.Sqrt(delta)) / (2 * a255);
 
                     Console.WriteLine("Pt co 2 nghiem x1={0} , x2={1}: ", x1, x2);
                 }
@@ -36,37 +36,37 @@ namespace btđiemanh26_4_bt
 
         class ptb2
         {
-            private double a210;
-            private double b210;
-            private double c210;
+            private double a255;
+            private double b255;
+            private double c255;
 
             public ptb2()
             {
             }
 
-            public ptb2(double a210, double b210, double c210)
+            public ptb2(double a255, double b255, double c255)
             {
-                A = a210;
-                B = b210;
-                C = c210;
+                A = a255;
+                B = b255;
+                C = c255;
             }
 
             public double A { get; set; }
             public double B { get; set; }
             public double C { get; set; }
 
-            public double delta(double a210, double b210, double c210)
+            public double delta(double a255, double b255, double c255)
             {
-                return b210 * b210 - 4 * a210 * c210;
+                return b255 * b255 - 4 * a255 * c255;
             }
 
-            public int nghiem(double delta210)
+            public int nghiem(double delta255)
             {
-                if (delta210 < 0)
+                if (delta255 < 0)
                 {
                     return -1;
                 }
-                else if (delta210 == 0)
+                else if (delta255 == 0)
                 {
                     return 0;
                 }
