f0ef0b25efb421609118211b3550f582e251624f
longbachviet <longbachviet@gmail.com>
2022-04-26 08:06:06 +0700

    a

--- diemdanh26_4/diemdanh26_4/Program.cs
+++ diemdanh26_4/diemdanh26_4/Program.cs
@@ -5,8 +5,62 @@ namespace diemdanh26_4
     class Program
     {
         static void Main(string[] args)
+        { 
+
+                Cat cat = new Cat();
+                cat.eat();
+                cat.makeSound();
+                cat.run();
+                Bird bird = new Bird();
+                bird.eat();
+                bird.makeSound();
+                bird.fly();
+                Console.ReadKey();
+            }
+        }
+        class Animal
         {
-            Console.WriteLine("Hello World!");
+            public virtual void eat()
+            {
+                Console.WriteLine("Thuc an");
+            }
+            public virtual void makeSound()
+            {
+                Console.WriteLine("");
+            }
+        }
+        class Cat : Animal
+        {
+            public void run()
+            {
+                Console.WriteLine("Meo chay nhanh");
+
+            }
+            public override void eat()
+            {
+                Console.WriteLine("Meo an ca");
+            }
+            public override void makeSound()
+            {
+                Console.WriteLine("Meo keu meo meo");
+            }
+        }
+        class Bird : Animal
+        {
+            public void fly()
+            {
+                Console.WriteLine("Chim bay cao");
+
+            }
+            public override void eat()
+            {
+                Console.WriteLine("Chim an sau");
+            }
+            public override void makeSound()
+            {
+                Console.WriteLine("Chim an con trung");
+            }
         }
     }
-}
+
+
