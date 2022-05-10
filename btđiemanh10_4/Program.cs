ad9238fcb8817801f9c8984058b716c08b4141d3
longbachviet <longbachviet@gmail.com>
2022-05-10 07:37:54 +0700

    a

---
+++ btđiemanh10_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace btđiemanh10_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+        }
+
+    }
+}
